49cba445d81ce9b9ca27cccc15b8b1e749f83c56
FellaCarz <62653731+FellaCarz@users.noreply.github.com>
2020-04-14 21:22:15 +0530

    Updates

---
+++ precache-manifest.7f31f813921c7d0774518a7f65846fdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a1429bcd4b6f9f64330ce7c3be872b",
+    "url": "/fellacarzapp/index.html"
+  },
+  {
+    "revision": "0ad5858d0c5e1a8a4eae",
+    "url": "/fellacarzapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfa9172c62c7d253fd36",
+    "url": "/fellacarzapp/static/js/2.512e7d98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fellacarzapp/static/js/2.512e7d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad5858d0c5e1a8a4eae",
+    "url": "/fellacarzapp/static/js/main.cb50b7ea.chunk.js"
+  },
+  {
+    "revision": "58f159072f20da3ec822",
+    "url": "/fellacarzapp/static/js/runtime-main.fee17794.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fellacarzapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
